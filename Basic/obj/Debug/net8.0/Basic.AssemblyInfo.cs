f061d96f3b35346377b55764a76db16d58f080dc
Sedefnur Kayran <109733884+sedefnurkayran@users.noreply.github.com>
2024-07-14 10:10:11 +0300

    .

--- Basic/obj/Debug/net8.0/Basic.AssemblyInfo.cs
+++ Basic/obj/Debug/net8.0/Basic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3fb9926b537ecdf9bf9cda76fb1e2242370d3e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8b7aef75d89b2b2d4a3f824b975fd359f07982b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
